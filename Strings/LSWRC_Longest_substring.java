40553933953f4fd08d43d9fcf353843341e131d8
bhushan_kumar <kumar.bbhavsar@gmail.com>
2025-05-27 03:07:53 -0500

    string/sliding window - longest substring withouth repeating characters

---
+++ Strings/LSWRC_Longest_substring.java
@@ -0,0 +1,47 @@
+package Strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LSWRC_Longest_substring {
+
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0 ;
+        int right = 0;
+        int maxLenght = 0 ;
+
+        Set<Character> charSet = new HashSet<>();
+        int n= s.length();
+        for(right =0 ; right < n; right ++ ) {
+
+            if(!charSet.contains(s.charAt(right))) {
+                charSet.add(s.charAt(right));
+                maxLenght = Math.max(maxLenght, right-left+1);
+            } else {
+
+                while (charSet.contains(s.charAt(right))) {
+                    charSet.remove(s.charAt(left));
+                    left ++;
+                }
+                charSet.add(s.charAt(right));
+            }
+
+
+        }
+
+        return maxLenght;
+    }
+
+    public static void main (String args[]) {
+
+        LSWRC_Longest_substring lswrc_longest_substring = new LSWRC_Longest_substring();
+
+        int result  = lswrc_longest_substring.lengthOfLongestSubstring("abcabccbb");
+
+        System.out.println("the result is " + result);
+
+
+
+
+    }
+}
